6db8353cfccf95ad655f734421f1fae64f89e17c
adityakaiser <singhadityakaiser@gmail.com>
2026-06-09 21:54:33 +0530

    Add files via upload

---
+++ file3.py
@@ -0,0 +1,23 @@
+L=[4,5,1,2,9,7,10,8]
+count=0
+for i in L:
+    count +=i
+
+avg=count/len(L)
+print("sum",count)
+print("avg is:",avg)
+L.sort()
+print(L)
+print("smallest element is",L[0])
+print("largest element is",L[-1])
+
+
+
+
+
+
+
+
+
+
+
